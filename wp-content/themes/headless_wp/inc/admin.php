69aa525abfb8ca3ba8241481088284075ebc47fc
Ben Townshend <ben.townshend@craftworkdigital.com>
2021-01-14 15:23:55 +0000

    added plugins

--- wp-content/themes/headless_wp/inc/admin.php
+++ wp-content/themes/headless_wp/inc/admin.php
@@ -4,8 +4,8 @@
  */
 if (function_exists('acf_add_options_page')) {
     acf_add_options_page([
-        'page_title' 	=> 'CIA Landlords Settings',
-        'menu_title' 	=> 'CIA Landlords',
+        'page_title' 	=> 'Gatsby Settings',
+        'menu_title' 	=> 'Gatsby',
         'menu_slug' 	=> 'cia',
         'capability' 	=> 'edit_posts',
         'redirect' 	    => false,
